21f188dfcd237dedfc6f3eeae8e80a6a57e4c575
zekroTJA <riho@live.de>
2023-08-23 23:35:05 +0200

    update mostplayed handler to run all updates at once

--- handlers/api/auto/mostplayed.rs
+++ handlers/api/auto/mostplayed.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
-    let time_range = expect!(get_query_param(&req, "time_range"));
+    let time_ranges = expect!(get_query_param(&req, "time_ranges")).unwrap_or("short".into());
     let name = expect!(get_query_param(&req, "name"));
 
     let controller = expect!(UnauthorizedController::from_env());
@@ -24,28 +24,43 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
 
     let controller = expect!(controller.authorize_with_token(refresh_token).await);
 
-    let store_key = format!(
-        "spotify_automation_playlist_id:{}",
-        time_range.as_deref().unwrap_or("unspecified")
-    );
+    let mut ids = Vec::with_capacity(3);
 
-    let playlist_id = expect!(db.get(&store_key));
+    for time_range in time_ranges.split(',').map(str::trim) {
+        let store_key = format!("spotify_automation_playlist_id:{}", time_range);
 
-    let id = expect!(
-        controller
-            .update_top_songs_playlist(
-                playlist_id.as_deref(),
-                name.as_deref().unwrap_or("Current Top Songs"),
-                time_range
-            )
-            .await
-    );
+        let playlist_id = expect!(db.get(&store_key));
 
-    if playlist_id.is_none() {
-        expect!(db.set(&store_key, &id.to_string()));
+        let id = expect!(
+            controller
+                .update_top_songs_playlist(
+                    playlist_id.as_deref(),
+                    &format!(
+                        "{} ({} Term)",
+                        name.as_deref().unwrap_or("Current Top Songs"),
+                        title(time_range)
+                    ),
+                    Some(time_range)
+                )
+                .await
+        );
+
+        if playlist_id.is_none() {
+            expect!(db.set(&store_key, &id.to_string()));
+        }
+
+        ids.push(id.to_string());
     }
 
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(Body::Text(format!("updated playlist with id {id}")))?)
+        .body(Body::Text(format!("updated playlists: {}", ids.join(", "))))?)
+}
+
+fn title(v: &str) -> String {
+    if v.is_empty() {
+        return "".into();
+    }
+    let first = v.chars().next().unwrap().to_uppercase();
+    format!("{first}{}", &v[1..])
 }
